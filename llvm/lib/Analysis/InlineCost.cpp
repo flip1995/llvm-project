ebe67669d72397454edd58d2fa7d3b65a15ea748
Philipp Krones <philipp.krones@embecosm.com>
2021-07-14 12:21:40 +0100

    [Inliner] Make the CallPenalty configurable
    Tests with multiple benchmarks, like Embench [1], showed that the
    CallPenalty magic number has the most influence on inlining decisions
    when optimizing for size.
    
    On the other hand, there was no good default value for this parameter.
    Some benchmarks profited strongly from a reduced call penalty. On
    example is the picojpeg benchmark compiled for RISC-V, which got 6%
    smaller with a CallPenalty of 10 instead of 12. Other benchmarks
    increased in size, like matmult.
    
    This commit makes the compromise of turning the magic number constant of
    CallPenalty into a configurable value. This introduces the flag
    `--inline-call-penalty`. With that flag users can fine tune the inliner
    to their needs.
    
    The CallPenalty constant was also used for loops. This commit replaces
    the CallPenalty constant with a new LoopPenalty constant that is now
    used instead.
    
    This is a slimmed down version of https://reviews.llvm.org/D30899
    
    [1]: https://github.com/embench/embench-iot
    
    Differential Revision: https://reviews.llvm.org/D105976

--- llvm/lib/Analysis/InlineCost.cpp
+++ llvm/lib/Analysis/InlineCost.cpp
@@ -113,6 +113,10 @@ static cl::opt<int> HotCallSiteRelFreq(
              "entry frequency, for a callsite to be hot in the absence of "
              "profile information."));
 
+static cl::opt<int> CallPenalty(
+    "inline-call-penalty", cl::Hidden, cl::init(25),
+    cl::desc("Call penalty that is applied per callsite when inlining"));
+
 static cl::opt<bool> OptComputeFullInlineCost(
     "inline-cost-full", cl::Hidden, cl::init(false), cl::ZeroOrMore,
     cl::desc("Compute the full inline cost of a call site even when the cost "
@@ -551,7 +555,7 @@ class InlineCostCallAnalyzer final : public CallAnalyzer {
     addCost(LoadEliminationCost);
     LoadEliminationCost = 0;
   }
-  void onCallPenalty() override { addCost(InlineConstants::CallPenalty); }
+  void onCallPenalty() override { addCost(CallPenalty); }
   void onCallArgumentSetup(const CallBase &Call) override {
     // Pay the price of the argument setup. We account for the average 1
     // instruction per call argument setup here.
@@ -586,7 +590,7 @@ class InlineCostCallAnalyzer final : public CallAnalyzer {
       }
     } else
       // Otherwise simply add the cost for merely making the call.
-      addCost(InlineConstants::CallPenalty);
+      addCost(CallPenalty);
   }
 
   void onFinalizeSwitch(unsigned JumpTableSize,
@@ -826,7 +830,7 @@ class InlineCostCallAnalyzer final : public CallAnalyzer {
           continue;
         NumLoops++;
       }
-      addCost(NumLoops * InlineConstants::CallPenalty);
+      addCost(NumLoops * InlineConstants::LoopPenalty);
     }
 
     // We applied the maximum possible vector bonus at the beginning. Now,
@@ -986,8 +990,7 @@ private:
   }
 
   void onCallPenalty() override {
-    increment(InlineCostFeatureIndex::CallPenalty,
-              InlineConstants::CallPenalty);
+    increment(InlineCostFeatureIndex::CallPenalty, CallPenalty);
   }
 
   void onCallArgumentSetup(const CallBase &Call) override {
@@ -1085,7 +1088,7 @@ private:
         if (DeadBlocks.count(L->getHeader()))
           continue;
         increment(InlineCostFeatureIndex::NumLoops,
-                  InlineConstants::CallPenalty);
+                  InlineConstants::LoopPenalty);
       }
     }
     set(InlineCostFeatureIndex::DeadBlocks, DeadBlocks.size());
@@ -2672,7 +2675,7 @@ int llvm::getCallsiteCost(CallBase &Call, const DataLayout &DL) {
     }
   }
   // The call instruction also disappears after inlining.
-  Cost += InlineConstants::InstrCost + InlineConstants::CallPenalty;
+  Cost += InlineConstants::InstrCost + CallPenalty;
   return Cost;
 }
 
